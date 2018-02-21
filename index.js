dc1c49fc218c11088521a74f6f8738d61e4f9e5b
Marc Ferguson <marc@fergytech.com>
2018-02-21 07:07:06 -0500

    A movable player cube.

--- index.js
+++ index.js
@@ -3,34 +3,60 @@
 const FPS = 30;
 let textX = 50;
 let textY = 50;
-const CANVAS_WIDTH = 100;
-const CANVAS_HEIGHT = 100;
+const CANVAS_WIDTH = 480;
+const CANVAS_HEIGHT = 300;
+const MOVEMENT = 6;
 
-let gameCanvas = document.createElement('canvas');
-let context;
+let canvasElement = document.createElement('canvas');
+let canvas = canvasElement.getContext('2d');
 
-gameCanvas.width = 480;
-gameCanvas.height = 300;
+canvasElement.width = CANVAS_WIDTH;
+canvasElement.height = CANVAS_HEIGHT;
 
-context = gameCanvas.getContext('2d');
+document.body.appendChild(canvasElement);
 
-context.fillStyle = 'green';
-context.fillRect(10, 10, CANVAS_WIDTH, CANVAS_HEIGHT);
+// player object
+let player = {
+  color: "#00A",
+  x: 50,
+  y: 50,
+  width: 32,
+  height: 32,
+  draw: function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
 
-document.body.appendChild(gameCanvas);
-
+// functions
 setInterval(function() {
   update();
   draw();
 }, 1000/FPS);
 
+document.addEventListener("keydown", function(event) {
+  const keyName = event.key;
+  switch(keyName) {
+    case "ArrowUp":
+      player.y -= MOVEMENT;
+      break;
+    case "ArrowRight":
+      player.x += MOVEMENT;
+      break;
+    case "ArrowDown":
+      player.y += MOVEMENT;
+      break;
+    case "ArrowLeft":
+      player.x -= MOVEMENT;
+  }
+});
+
 function update() {
-  textX += 1;
-  textY += 1;
+  player.x = player.x.clamp(0, CANVAS_WIDTH - player.width);
+  player.y = player.y.clamp(0, CANVAS_HEIGHT - player.height);
 }
 
 function draw() {
-  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  context.fillStyle = "#000";
-  context.fillText("Sup Bro!", textX, textY);
+  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  player.draw();
 }
